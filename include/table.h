1d75bde39c6aa03a39c52f252d14609a94efe1ed
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2022-06-09 01:38:45 +0200

    separated row and table structs into own files

---
+++ include/table.h
@@ -0,0 +1,38 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <stdint.h>
+#include <string.h>
+// for `offsetof()`
+#include <stddef.h>
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+// `(Struct*)0` => struct pointer
+// `(((Struct*)0)->Attribute)` => pointer to that specific attribute of a given struct
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+
+// Row data sizes
+static const uint32_t ID_SIZE  = size_of_attribute(Row, id);
+static const uint32_t USERNAME_SIZE  = size_of_attribute(Row, username);
+static const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+
+// Row data memory offset
+static const uint32_t ID_OFFSET = offsetof(Row, id);
+static const uint32_t USERNAME_OFFSET = offsetof(Row, username);
+static const uint32_t EMAIL_OFFSET = offsetof(Row, email);
+static const uint32_t ROW_SIZE = ID_SIZE+USERNAME_SIZE+EMAIL_SIZE;
+
+
+void row_serialization(Row* source, void* destination);
+void row_deserialization(void* source, Row* destination);
+
+#endif
---
+++ src/table.c
@@ -0,0 +1,14 @@
+#include "table.h"
+
+void row_serialization(Row* source, void* destination) {
+    memcpy(destination+ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination+USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination+EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void row_deserialization(void* source, Row* destination) {
+    // passing addresses to memcpy (destination, source, size_t n)
+    memcpy(&(destination->id), source+ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source+USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source+EMAIL_OFFSET, EMAIL_SIZE);
+}
