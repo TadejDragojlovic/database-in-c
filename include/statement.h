b6a871807a316770a6ddb0747ee14582c61f1459
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2022-06-10 16:38:46 +0200

    split functions for statement execution,  statement somewhat added

--- include/statement.h
+++ include/statement.h
@@ -2,12 +2,20 @@
 #define STATEMENT_H
 
 #include "buffer.h"
+#include "table.h"
 
 // success/failure of statement preparation
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
+
+// statement execution results
+typedef enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
 
 // type of statement
 typedef enum {
@@ -18,11 +26,15 @@ typedef enum {
 // statement struct
 typedef struct {
     StatementType type;
+    Row row_to_insert; // this is only used by `insert` statement
 } Statement;
 
+void print_row(Row* row);
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+ExecuteResult execute_statement(Statement* statement, Table* table);
 
-void execute_statement(Statement* statement);
-
+ExecuteResult execute_insert(Statement* prepared_statement, Table* table);
+ExecuteResult execute_select(Statement* prepared_statement, Table* table);
 
 #endif
--- src/statement.c
+++ src/statement.c
@@ -6,13 +6,11 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
         /* inserting into our 1-table database might look something like this `insert 1 placeholder_name placeholder_email` */
         /* we need to *parse* arguments for this specific statement */
 
-        printf("INPUT: %s\n", input_buffer->buffer);
-
         /* parsing argments and storing them to the row_to_insert Row */
         int args = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), 
                           statement->row_to_insert.username, statement->row_to_insert.email);
 
-        printf("%d\n", args);
+        // printf("%d\n", args);
 
         if(args != 3) {
             printf("SYNTAX ERROR.\n");
@@ -20,8 +18,8 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
         }
 
         printf("%d %d %d\n", ID_SIZE, USERNAME_SIZE, EMAIL_SIZE);
-        printf("%d %d %d\n", ID_OFFSET, USERNAME_OFFSET, EMAIL_OFFSET);
-        printf("TOTAL ROW SIZE: %d\n", ROW_SIZE);
+        // printf("%d %d %d\n", ID_OFFSET, USERNAME_OFFSET, EMAIL_OFFSET);
+        // printf("TOTAL ROW SIZE: %d\n", ROW_SIZE);
 
         statement->type = STATEMENT_INSERT;
         return PREPARE_SUCCESS;
@@ -33,15 +31,40 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+
 // VIRTUAL MACHINE PART
 // after compiled statement, this function executes given statements
-void execute_statement(Statement* statement) {
+ExecuteResult execute_statement(Statement* statement, Table* table) {
     switch(statement->type) {
         case(STATEMENT_INSERT):
-            printf("Compiler reads INSERT\n");
-            break;
+            return execute_insert(statement, table);
         case(STATEMENT_SELECT):
-            printf("Compiler reads SELECT\n");
-            break;
+            return execute_select(statement, table);
     }
+}
+
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+    if (table->row_count >= TABLE_MAXIMUM_ROWS)
+        return EXECUTE_TABLE_FULL;
+
+    Row* row_to_insert = &(statement->row_to_insert);
+
+    row_serialization(row_to_insert, row_slot(table, table->row_count));
+    table->row_count++;
+
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement* prepared_statement, Table* table) {
+    Row row;
+    for(int i=0;i<table->row_count;i++) {
+        row_deserialization(row_slot(table, i), &row);
+        print_row(&row);
+    }
+
+    return EXECUTE_SUCCESS;
+}
+
+void print_row(Row* row) {
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
