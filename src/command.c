36ca74a3bc4669faa6d4916bd7c1ea226b755773
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2022-06-14 19:29:48 +0200

    added db_close() function that closes and frees the memory all at once

--- src/command.c
+++ src/command.c
@@ -2,8 +2,7 @@
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     if(strncmp(input_buffer->buffer, ".exit", 5) == 0) {
-        free_input_buffer(input_buffer);
-        free_table(table);
+        db_close(table);
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED;
