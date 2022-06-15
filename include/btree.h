50c33bccad2b53f4dc2d39f290d4e67e4bcb8a7e
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2022-06-15 15:20:28 +0200

    header file for the btree data structure

---
+++ include/btree.h
@@ -0,0 +1,59 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+typedef enum {NODE_INTERNAL, NODE_LEAF} NodeType;
+
+
+/* Node header layout */
+
+// common header for internal nodes
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_OFFSET + NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint32_t COMMON_HEADER_SIZE =  NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+// for leaf nodes
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const unit32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+
+
+/* Leaf node body layout */
+
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+
+
+/* returns a pointer to the node's cell number value */
+uint32_t* leaf_node_num_cells(void* node) {
+    return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+/* returns a pointer to the block of memory where a certain (inputed by argument 'cell_num') cell is stored */
+void* leaf_node_cell(void* node, uint32_t cell_num) {
+    return node + LEAF_NODE_HEADER_SIZE + cell_num*LEAF_NODE_CELL_SIZE;
+}
+
+/* returns a pointer to inputed cell's key in the memory */
+uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num);
+}
+
+/* returns a pointer to the block of memory where value of a certain cell is stored (inputed by argument 'cell_num') */
+void* leaf_node_value(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void* node) { *leaf_node_num_cells(node) = 0; }
+
+#endif
