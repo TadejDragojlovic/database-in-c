ed48c6afd559a37d1e09ffba82077a16540bfadb
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2022-06-16 14:49:06 +0200

    print constants and print leaf node information

--- src/btree.c
+++ src/btree.c
@@ -1,5 +1,6 @@
 #include "btree.h"
 
+/* print all constants in the 'btree.h' file */
 void print_constants() {
     printf("ROW_SIZE: %d\n", ROW_SIZE);
     printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_HEADER_SIZE);
@@ -7,6 +8,16 @@ void print_constants() {
     printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
     printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
     printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+/* print information about given node structure */
+void print_leaf_node(void* node) {
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    printf("Leaf (size %d)\n", num_cells);
+    for (uint32_t i=0; i<num_cells; i++) {
+        uint32_t key = *leaf_node_key(node, i);
+        printf("    - %d : %d\n", i, key);
+    }
 }
 
 /* returns a pointer to the node's cell number value */
